a6f40df47ac979e663e12647ce4f4a420e283d7f
Hengyuan Hu <kevin.huhy@gmail.com>
2016-10-17 14:38:52 -0400

    Change mean-pixel subtract and norm data with dataset stats.

--- data_cifar10.py
+++ data_cifar10.py
@@ -2,9 +2,11 @@ import cPickle
 import numpy as np
 import os
 from keras.utils.np_utils import to_categorical
+from keras.datasets import cifar10
 
+COLOR_MEAN_RGB = np.array([125.3, 123.0, 113.9]).reshape(3, 1, 1)
+COLOR_STD_RGB  = np.array([63.0,  62.1,  66.7]).reshape(3, 1, 1)
 
-IMAGENET_MEAN_RGB = np.array([123.151630838, 115.902882574, 103.062623801]).reshape(3, 1, 1)
 DATA_ROOT = os.path.join(os.path.dirname(__file__), 'cifar-10-batches-py')
 TRAIN_BATCHES = [os.path.join(DATA_ROOT, batch) for batch in
                  ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']]
@@ -17,7 +19,7 @@ def _load_batch(filename):
     xs = d['data']
     ys = to_categorical(np.array(d['labels']), NUM_CLASSES)
     xs = xs.reshape(-1, 3, 32, 32)
-    xs = (xs - IMAGENET_MEAN_RGB) / 255.0
+    xs = (xs - COLOR_MEAN_RGB) / COLOR_STD_RGB
     return xs, ys
 
 
@@ -30,8 +32,8 @@ def load_cifar10(train_batches=TRAIN_BATCHES, test_batch=TEST_BATCH):
         train_xs = np.vstack((train_xs, xs))
         train_ys = np.vstack((train_ys, ys))
     test_xs, test_ys = _load_batch(test_batch)
-    return train_xs, train_ys, test_xs, test_ys
-    # return train_xs.swapaxes(1, 3), train_ys, test_xs.swapaxes(1, 3), test_ys
+
+    return (train_xs, train_ys), (test_xs, test_ys)
 
 
 def augment_batch(batch, pad_dim=4):
@@ -48,5 +50,5 @@ def augment_batch(batch, pad_dim=4):
 
 
 if __name__ == '__main__':
-    train_xs, train_ys, test_xs, test_ys = load_cifar10()
+    (train_xs, train_ys), (test_xs, test_ys) = load_cifar10()
     print 'Loaded dataset:', train_xs.shape, train_ys.shape
